70d1de277938263e3b67a663597280a669b1fc7b
JasonLev <jasond.levy@gmail.com>
2013-12-22 00:12:27 -0800

    initial commit; include rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BevSelector::Application.config.secret_token = '7588a74b1b60df1ffcf9d6deec9dd2343366837de07650f0506402dff49d6b28380a357c7f967b1b5d6538bc963598287cbf522860e7bbc0e59da883a2d6f4c0'
