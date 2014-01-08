397d47ab00b964ec354fc7b38c9cc3b1b525a2ba
JasonLev <jasond.levy@gmail.com>
2014-01-07 16:14:07 -0800

    add action mailer

---
+++ app/mailers/bev_mailer.rb
@@ -0,0 +1,9 @@
+class BevMailer < ActionMailer::Base
+  default :from => "contact@bev-selector.com"
+
+  def registration_confirmation(user)
+    @user = current_user
+    @friend = current_user.friends.last
+    mail(:to => "#{@friend.name} <#{@friend.email}>", :subject => "#{@user} ordered a drink with Bev Selector!")
+  end
+end
