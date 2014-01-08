55413c05aa881404469d08f63de5ecae70771f21
JasonLev <jasond.levy@gmail.com>
2014-01-07 16:51:05 -0800

    edit mailer; edit config settings for Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,8 @@ BevSelector::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # Yes, we care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -36,5 +36,17 @@ BevSelector::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "bev-selector.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
 end
